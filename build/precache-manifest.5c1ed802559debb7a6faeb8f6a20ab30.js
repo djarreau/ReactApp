c44c12c621b0d2946baa58e9613a630550ba59f1
djarreau <djgco360@gmail.com>
2020-08-24 20:20:25 -0400

    Second commit

---
+++ build/precache-manifest.5c1ed802559debb7a6faeb8f6a20ab30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016df8af7a290ea6626f1af9dba5b0f8",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "02e5df20887f78f1f987",
+    "url": "/ReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/ReactApp/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e5df20887f78f1f987",
+    "url": "/ReactApp/static/js/main.ae929490.chunk.js"
+  },
+  {
+    "revision": "faf4aa599cc1eb5e3d16",
+    "url": "/ReactApp/static/js/runtime-main.a0f41cb0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
